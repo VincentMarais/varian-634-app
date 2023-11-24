82375509f65cd9b8ca9e38756490be6173e945cf
Vincent Marais <vincent.travail12@gmail.com>
2023-11-24 22:18:53 +0100

    update(correct directory_creation)

--- client/python/core/utils/directory_creation.py
+++ client/python/core/utils/directory_creation.py
@@ -1,39 +1,55 @@
+"""This program will create directory file with python."""
+
 import os
 import datetime
 
-def detection_exitance_directory(chemin): # Path chemin d'accès
-    chemin=os.path.join(chemin)
-    if not os.path.exists(chemin):
+def detection_exitance_directory(path): # Path chemin d'accès
+    """
+    Look if the directory exist
+    """
+    path=os.path.join(path)
+    if not os.path.exists(path):
         return False
 
 def directory_year_month_day():
+    """
+    Create a directory with the current year_month_day    
+    """
     current_date = datetime.datetime.now()
-    current_year, current_month, current_day = current_date.strftime("%Y") , current_date.strftime("%m_%Y"), current_date.strftime("%d_%m_%Y") 
-        
-    chemin=os.path.join("./experiments","experiments_"+ current_year, "experiments_"+ current_month , "experiments_"+current_day)
-    if  detection_exitance_directory(chemin)==False:
-        os.makedirs(chemin)
-        print("Le répertoire à été créé : ", chemin)
+    current_year = current_date.strftime("%Y")
+    current_month = current_date.strftime("%m_%Y")
+    current_day = current_date.strftime("%d_%m_%Y")
+    path = os.path.join(
+        "./experiments",
+        "experiments_" + current_year,
+        "experiments_" + current_month,
+        "experiments_" + current_day)    
+    if  detection_exitance_directory(path) is False:
+        os.makedirs(path)
+        print("Le répertoire à été créé : ", path)
     else:
-            print("Le répertoire existe déjà :", chemin)
+        print("Le répertoire existe déjà :", path)
 
-    return chemin
+    return path
 
 def creation_directory_date_slot():
-    Taille_de_fente=input("Taille de fente : Fente_2nm, Fente_1nm , Fente_0_5nm, Fente_0_2nm : ")
-    date_aujourdhui = datetime.date.today()
-    Date = date_aujourdhui.strftime("%d_%m_%Y")
-    chemin= directory_year_month_day()
-    chemin = os.path.join(chemin, Taille_de_fente)
+    """
+    Creating a directory with the length of the slot used in the experiment.
+    
+    """
+    slot_size=input("Taille de fente : Fente_2nm, Fente_1nm , Fente_0_5nm, Fente_0_2nm : ")
+    date_today = datetime.date.today()
+    date = date_today.strftime("%d_%m_%Y")
+    path= directory_year_month_day()
+    path = os.path.join(path, slot_size)
 
     # Vérifier si le répertoire existe déjà
-    if not os.path.exists(chemin):
-        # Créer le répertoire en utilisant le chemin d'accès
-        os.makedirs(chemin)
-        print("Répertoire créé avec succès :", chemin)
+    if not os.path.exists(path):
+        # Créer le répertoire en utilisant le path d'accès
+        os.makedirs(path)
+        print("Répertoire créé avec succès :", path)
     else:
-        print("Le répertoire existe déjà :", chemin)
+        print("Le répertoire existe déjà :", path)
 
-    return chemin, Date, Taille_de_fente
-
-
+    return path, date, slot_size
+# End-of-file (EOF)
