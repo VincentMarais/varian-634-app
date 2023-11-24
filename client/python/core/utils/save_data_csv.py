36ecac06fe88c3a38d0421f4dd46862d7691f855
Vincent Marais <vincent.travail12@gmail.com>
2023-11-24 23:14:40 +0100

    fix(save_data_csv.py)

--- client/python/core/utils/save_data_csv.py
+++ client/python/core/utils/save_data_csv.py
@@ -1,10 +1,23 @@
+"""
+Program transform the python list on csv file
+"""
+
 import csv
+import itertools
 
+def save_data_csv(path, file_name, data_list, title_list):
 
-# Fonction pour écrire les données dans un fichier CSV
-def save_data_csv(nom_fichier, Liste_longueurs_d_onde, Liste_tensions, Liste_pas_vis, titre_1, titre_2, titre_3): # nom_Fichier: str / Liste_longueurs_d_onde, Liste_tensions: Liste / titre_1, titre_2: str
-    with open(nom_fichier, 'w', newline='') as fichier_csv:
-        writer = csv.writer(fichier_csv)
-        writer.writerow([titre_1, titre_2,titre_3])
-        for i in range(len(Liste_longueurs_d_onde)):
-            writer.writerow([Liste_longueurs_d_onde[i], Liste_tensions[i], Liste_pas_vis[i]])
+    """
+    Program transform the python list on csv file
+
+    """
+    path_file = path + '/' + file_name
+    # Transposer la liste de données
+    data_transposed = list(itertools.zip_longest(*data_list))
+    with open(path_file, 'w', newline='', encoding='utf-8') as file_csv:
+        writer = csv.writer(file_csv)
+        # Écrire les titres en tant que première ligne
+        writer.writerow(title_list)
+        # Écrire les données transposées
+        for row in data_transposed:
+            writer.writerow(row)
