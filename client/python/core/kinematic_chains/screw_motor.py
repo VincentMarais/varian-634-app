905bfab190111eb942183ef5cb26d8016b58e70e
Vincent Marais <vincent.travail12@gmail.com>
2023-12-09 15:01:37 +0100

    Update(syntax screw_motor)

--- client/python/core/kinematic_chains/screw_motor.py
+++ client/python/core/kinematic_chains/screw_motor.py
@@ -1,10 +1,10 @@
 """
-This program pilot the motor who translate the screw and le réseau de diffraction par réflexion du VARIAN 634
+This program controls the motor that drives the screw and the reflection 
+diffraction grating of the VARIAN 634
 """
 
 import time
-import serial
-from pyfirmata import Arduino, util, INPUT
+from pyfirmata import util, INPUT
 
 # CARACTERISTIQUE GRBL MOTEUR
 
@@ -20,7 +20,7 @@ def state_screw_motor(arduino_motors):
     arduino_motors.write(g_code.encode())
     return arduino_motors.read(40) # 10: On lit 10 caractère dans le serial
 
-def grbl_parameter_screw_motor(arduino_motors):    
+def grbl_parameter_screw_motor(arduino_motors):
     """
     Entree : arduino_motors
 
@@ -37,33 +37,47 @@ def grbl_parameter_screw_motor(arduino_motors):
 # initialization of the motor
 
 def modify_screw_translation_speed(arduino_motors, screw_translation_speed):
+    """
+    Modifie la vitesse de translation de la vis
+    """
     g_code = '$110=' + str(screw_translation_speed) + '\n'
     arduino_motors.write(g_code.encode())
     time.sleep(0.5)
 
 
 def initialisation_motor_screw(arduino_motors, screw_translation_speed):
+    """
+    Initialise la vis qui déplace le réseau de diffraction pour le début de l'expérience
+    """
     g_code= 'G90'+ '\n' # Le moteur se déplace en relatif
     arduino_motors.write(g_code.encode())
     time.sleep(0.5)
     modify_screw_translation_speed(arduino_motors, screw_translation_speed)
 
-# screw cinematics 
-        
-def move_screw(arduino_motors, screw_course, screw_translation_speed): # Fonction qui pilote le moteur
-        modify_screw_translation_speed(arduino_motors, screw_translation_speed)      
-        g_code= 'G90\n' + 'G0X' + str(screw_course) + '\n' # Le moteur ce déplace en relatif
-        arduino_motors.write(g_code.encode())
-        
+# screw cinematics
 
-def return_screw(arduino_motors, screw_course, screw_translation_speed): 
-        modify_screw_translation_speed(arduino_motors, screw_translation_speed)
-        g_code= 'G91'+ 'G0X-' + str(screw_course) + '\n' # Le moteur ce déplace en relatif  
-        #Le moteur ce déplace linéairement de -pas_vis (retour_moteur_vis en arrière)
-        arduino_motors.write(g_code.encode())
+def move_screw(arduino_motors, screw_course, screw_translation_speed):
+    """
+    Déplace la vis qui déplace le réseau de diffraction
+    """
+    modify_screw_translation_speed(arduino_motors, screw_translation_speed)
+    g_code= 'G90\n' + 'G0X' + str(screw_course) + '\n' # Le moteur ce déplace en relatif
+    arduino_motors.write(g_code.encode())
+
+def return_screw(arduino_motors, screw_course, screw_translation_speed):
+    """
+    Déplacement arrière de la vis
+    """
+    modify_screw_translation_speed(arduino_motors, screw_translation_speed)
+    g_code= 'G91'+ 'G0X-' + str(screw_course) + '\n' # Le moteur ce déplace en relatif
+    #Le moteur ce déplace linéairement de -pas_vis (retour_moteur_vis en arrière)
+    arduino_motors.write(g_code.encode())
 
 
 def end_stop_state(arduino_end_stop, arduino_motors,screw_course, screw_translation_speed):
+    """
+    Donne l'état des capteurs de fin de course on niveau de la vis 
+    """
     arduino_end_stop.digital[2].mode = INPUT  # Modifié ici
 
 # Créer une instance d'Itérateur pour ne pas manquer les données entrantes
@@ -72,44 +86,23 @@ def end_stop_state(arduino_end_stop, arduino_motors,screw_course, screw_translat
 
 # Permettre à l'itérateur de démarrer
     time.sleep(1)
-    digital_value = arduino_end_stop.digital[2].read()    
+    digital_value = arduino_end_stop.digital[2].read()
     while digital_value is False:
         # Lire la valeur du port digital 3
         digital_value = arduino_end_stop.digital[2].read()
         print(digital_value)
-        move_screw(arduino_motors, screw_course, screw_translation_speed)    
+        move_screw(arduino_motors, screw_course, screw_translation_speed)
         digital_value = arduino_end_stop.digital[2].read()
         print(digital_value)
     g_code = '!'+'\n'
     arduino_motors.write(g_code.encode())
-   
-
 
 def position_moteur_x(arduino_motors):
+    """
+    Donne la position de la vis 
+    """
     # Demande la position actuelle du moteur selon l'axe X
     arduino_motors.write(b"?x\n")
     reponse = arduino_motors.readline().decode().strip()
     position_x = reponse.split(":")[1]
     return position_x
-# Test python (vous devez suivre la même procédure expliquer dans le code mirror_cuves_motor.py)
-# https://www.youtube.com/watch?v=IJER_8nqK-Y&t=16s&ab_channel=ZenToolworks (Vidéo pour le homing et regarder)
-# (Regarder https://www.youtube.com/watch?v=OpaUwWouyE0&ab_channel=MichaelKlements)
-
-
-# INITIALISATION MOTEUR:
-
-COM_PORT = 'COM3'
-BAUD_RATE = 115200
-INITIALIZATION_TIME = 2
-
-arduino_motors = serial.Serial(COM_PORT, BAUD_RATE)
-arduino_motors.write("\r\n\r\n".encode()) # encode pour convertir "\r\n\r\n" 
-time.sleep(INITIALIZATION_TIME)   # Attend initialisation un GRBL
-arduino_motors.flushInput()  # Vider le tampon d'entrée, en supprimant tout son contenu.
-
-arduino_end_stop=Arduino('COM6')
-g_code='$X' + '\n'
-arduino_motors.write(g_code.encode())
-
-end_stop_state(arduino_motors=arduino_motors,arduino_end_stop=arduino_end_stop, screw_course=-1, screw_translation_speed=10)
-#move_screw(arduino_motors=arduino_motors, screw_course=-1, screw_translation_speed=10)
